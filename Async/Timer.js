01bf7058bf748d06ed92a8af07787fae93c30da2
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-08-15 14:53:49 +0530

    Added: To demonstrate the async nature of Javascript

--- Async/Timer.js
+++ Async/Timer.js
@@ -1,1 +1,14 @@
-console.log("Welcome");
+//Uc-1 To demonstrate asynchronous nature of javascript
+function showTime() {
+    const date = new Date();
+    return date.getHours() + "Hrs: " + date.getMinutes() + " Mins: " + date.getSeconds() + " Secs ";
+}
+
+function showSessionExpire() {
+    console.log("Activity-B : Your Session expired at " + showTime());
+}
+
+
+console.log("Activity -A : Triggering Activity-B At " + showTime());
+setTimeout(showSessionExpire, 5000);
+console.log("Activity -A : Triggering Activity-B At " + showTime() + " will execute after 5 sec");
