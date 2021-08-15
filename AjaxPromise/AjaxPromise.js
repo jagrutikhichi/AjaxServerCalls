c5b3e97264c6a077370a47bd1e083788fa679c0e
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-08-15 16:15:57 +0530

    Added: To view Employee Data from JSON server using ajax and Promise

---
+++ AjaxPromise/AjaxPromise.js
@@ -0,0 +1,46 @@
+//UC-3 To view data from JSON server using Ajax and Promise  
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function makePromiseCall(methodType, url, async, data) {
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            // console.log("State changed called . Ready State :"+xhr.readyState+" Status :"+xhr.status);
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200 || xhr.status === 201) {
+                    resolve(xhr.responseText);
+                } else if (xhr.status >= 400) {
+                    reject({
+                        status: xhr.status,
+                        statusText: xhr.statusText
+                    })
+                    console.log("XHR Failed")
+                }
+            }
+        }
+        xhr.open(methodType, url, async);
+        console.log(methodType + " request sent to the server")
+        if (data) {
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(data));
+        } else {
+            xhr.send();
+        }
+    });
+}
+
+const getUrl = "http://localhost:3000/employee/1";
+makePromiseCall("GET", getUrl, true)
+    .then(response => console.log("Get User Data " + response))
+    .catch(error => console.log("Get Error Status : " + JSON.stringify(error)))
+
+const deleteUrl = "http://localhost:3000/employee/5";
+makePromiseCall("DELETE", deleteUrl, false)
+    .then(response => console.log("User Deleted " + response))
+    .catch(error => console.log("Delete Error Status : " + JSON.stringify(error)))
+
+const postUrl = "http://localhost:3000/employee";
+const empData = { "name": "Luther Thomas", "Salary": 500000 };
+makePromiseCall("POST", postUrl, true, empData)
+    .then(response => console.log("User Added " + response))
+    .catch(error => console.log("Post Error Status : " + JSON.stringify(error))
